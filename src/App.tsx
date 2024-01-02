3241943bf936bde480af416470bd1fa927987dbc
ericksantos2 <erick.s.g.barros@gmail.com>
2024-01-02 17:35:11 -0300

    movi o root do app para o arquivo main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,91 @@
-import { createRoot } from 'react-dom/client';
+import { createContext, useReducer, useState } from 'react';
+import styles from './styles/App.module.scss';
+import Comando from './components/Comando';
+import { FaRegSquarePlus } from 'react-icons/fa6';
+import NovoComando from './components/NovoComando';
+import { IoPencil, IoSettingsOutline } from 'react-icons/io5';
+import Configuracoes from './components/Configuracoes';
 
-const root = createRoot(document.querySelector('#root'));
+export type ComandosType = {
+  nome: string;
+  comando: string;
+  mostraTerminal: boolean;
+  fechaSozinho: boolean;
+  id: string;
+};
 
-export default function App() {
-  return (
-    <h2>Hello World</h2>
-  )
+export const AppContext = createContext(null);
+
+export type ComandosAction = {
+  type: string;
+  payload: string | ComandosType | ComandosType[];
+};
+
+function comandosReducer(state: ComandosType[], action: ComandosAction) {
+  if (action.type === 'Adicionar') {
+    const novoState = [...state, action.payload];
+    localStorage.setItem('comandos', JSON.stringify(novoState));
+    return novoState;
+  } else if (action.type === 'Remover') {
+    const novoState = [...state];
+    const index = novoState.findIndex((item) => item.id === action.payload);
+    if (index < 0) return state;
+    novoState.splice(index, 1);
+    localStorage.setItem('comandos', JSON.stringify(novoState));
+    return novoState;
+  } else {
+    return state;
+  }
 }
 
-root.render(
-  <App/>
-)
+export default function App() {
+  const [comandosState, comandosDispatch] = useReducer(
+    comandosReducer,
+    JSON.parse(localStorage.getItem('comandos')) || []
+  );
+  const [adicionando, setAdicionando] = useState(false);
+  const [editando, setEditando] = useState(false);
+  const [configurando, setConfigurando] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        comandos: { state: comandosState, dispatch: comandosDispatch },
+        app: { setAdicionando, editando, setConfigurando },
+      }}
+    >
+      <div className={styles.app}>
+        <div className={styles.comandos}>
+          {(comandosState as ComandosType[]).map((comando, index) => (
+            <Comando key={index} id={comando.id} nome={comando.nome} />
+          ))}
+        </div>
+        <div className={styles.botoes}>
+          <span
+            onClick={() => {
+              setAdicionando((value) => !value);
+            }}
+          >
+            <FaRegSquarePlus size={18} />
+          </span>
+          <span
+            onClick={() => {
+              setEditando((value) => !value);
+            }}
+          >
+            <IoPencil size={18} />
+          </span>
+          <span
+            onClick={() => {
+              setConfigurando((value) => !value);
+            }}
+          >
+            <IoSettingsOutline size={18} />
+          </span>
+        </div>
+        {adicionando && <NovoComando />}
+        {configurando && <Configuracoes />}
+      </div>
+    </AppContext.Provider>
+  );
+}
