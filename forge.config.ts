c8a4a9902b316c6e3fc2a63e80abb7594060577a
ericksantos2 <erick.s.g.barros@gmail.com>
2024-01-02 17:31:32 -0300

    coloquei o forge para buildar só pra linux por padrão

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,4 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -14,11 +10,19 @@ const config: ForgeConfig = {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  makers: [{
+    name: '@electron-forge/maker-zip',
+    config: {
+      options: {
+        icon: './images/icon.png'
+      }
+    }
+  }],
   plugins: [
     new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
       mainConfig,
+      devContentSecurityPolicy: "connect-src 'self' * 'unsafe-eval'",
       renderer: {
         config: rendererConfig,
         entryPoints: [
