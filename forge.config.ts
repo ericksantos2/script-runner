9b0f69405438fa776c932d6848a7628bd0a4dcd9
ericksantos2 <116085135+ericksantos2@users.noreply.github.com>
2024-02-23 22:12:15 -0300

    removi um plugin que não estava sendo usado e formatei o código

--- forge.config.ts
+++ forge.config.ts
@@ -1,25 +1,27 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import type { ForgeConfig } from "@electron-forge/shared-types";
+// import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
+import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
+import { mainConfig } from "./webpack.main.config";
+import { rendererConfig } from "./webpack.renderer.config";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [{
-    name: '@electron-forge/maker-zip',
-    config: {
-      options: {
-        icon: './images/icon.png'
-      }
-    }
-  }],
+  makers: [
+    {
+      name: "@electron-forge/maker-zip",
+      config: {
+        options: {
+          icon: "./images/icon.png",
+        },
+      },
+    },
+  ],
   plugins: [
-    new AutoUnpackNativesPlugin({}),
+    // new AutoUnpackNativesPlugin({}),
     new WebpackPlugin({
       mainConfig,
       devContentSecurityPolicy: "connect-src 'self' * 'unsafe-eval'",
@@ -27,11 +29,11 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/index.html',
-            js: './src/renderer.ts',
-            name: 'main_window',
+            html: "./src/index.html",
+            js: "./src/renderer.ts",
+            name: "main_window",
             preload: {
-              js: './src/preload.ts',
+              js: "./src/preload.ts",
             },
           },
         ],
